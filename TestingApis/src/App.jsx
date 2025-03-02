2dddeb481de52b19689aa8bca1ab0dd10241d316
davidojo1144 <davco1144@gmail.com>
2025-03-02 19:00:16 +0100

    Testing Apis

--- TestingApis/src/App.jsx
+++ TestingApis/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import CatData from './components/CatData'
 import Predict from './components/Predict'
+import Excuse from './components/Excuse'
 
 const App = () => {
   return (
-    <div className='container space-y-20 mt-10 bg-gray-300 rounded-2xl p-5 items-center justify-center' >
+    <div className='container overflow-x-hidden space-y-20 mt-10 bg-gray-300 rounded-2xl p-5 items-center justify-center' >
       <CatData/>
       <Predict/>
+      <Excuse/>
     </div>
   )
 }
---
+++ TestingApis/src/components/Excuse.jsx
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+const Excuse = () => {
+  const [generatedExcuse, setGeneratedExcuse] = useState("");
+
+  const fetchExcuses = (excuse) => {
+    axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}`)
+      .then((res) => {
+        setGeneratedExcuse(res.data[0].excuse); 
+      })
+      .catch((error) => {
+        console.error('Error fetching excuse:', error); 
+      });
+  };
+
+  return (
+    <div>
+      <div className='mb-10'>
+        <h1 className='text-center text-3xl font-bold pb-5'>GENERATE EXCUSES</h1>
+        <div className='text-center justify-center space-x-5'>
+          <button
+            className='border bg-green-400 w-32 h-10 rounded-full'
+            onClick={() => fetchExcuses("party")}
+          >
+            Party Excuse
+          </button>
+          <button
+            className='border bg-blue-400 w-32 h-10 rounded-full'
+            onClick={() => fetchExcuses("family")}
+          >
+            Family Excuse
+          </button>
+          <button
+            className='border bg-orange-400 w-32 h-10 rounded-full'
+            onClick={() => fetchExcuses("office")}
+          >
+            Office Excuse
+          </button>
+        </div>
+        <p className='text-center mt-5'>{generatedExcuse}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Excuse;
