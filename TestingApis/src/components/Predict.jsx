8b3c580e88718ccc765f21c37dddfada518fe431
davidojo1144 <davco1144@gmail.com>
2025-03-02 18:06:48 +0100

    Testing Apis

--- TestingApis/src/components/Predict.jsx
+++ TestingApis/src/components/Predict.jsx
@@ -18,12 +18,12 @@ return (
       <div className='gap-5 pt-10 text-center justify-center pb-5'>
         <h1 className='text-3xl font-bold pb-5'>PREDICT YOUR AGE</h1>
         <div className='flex text-center justify-center space-x-5 '>
-            <input className='w-48 rounded border h-10 pl-5'  type="text" placeholder='Enter your name' onChange={(event)=> setname(event.target.value)} />
+            <input className='w-48 rounded border h-10 pl-5'  type="text" placeholder='Enter your name' onChange={(event)=> {setname(event.target.value)}} />
             <button className='w-32 h-10 bg-blue-400 rounded-full mb-5' onClick={fetchdata}>Predict Age</button>
         </div>
-        <h1>Predicted Name: {predictedAge?.name}</h1>
+        <h1>Name: {predictedAge?.name}</h1>
         <h1>Predicted age: {predictedAge?.age}</h1>
-        <h1>Predicted count: {predictedAge?.count}</h1>
+        <h1>count: {predictedAge?.count}</h1>
     </div>
     </div>
   )
